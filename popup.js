98932a572247a9d98b9fbf34ba10af67c2834e8c
wildjun <yeshengme@gmail.com>
2016-12-09 17:02:00 +0800

    引入百度搜索查询中文转英文

--- popup.js
+++ popup.js
@@ -1,8 +1,7 @@
-
 function getFirstChildWithTagName(element, tagName) {
     var tagName = tagName.toUpperCase();
     var childs = element.childNodes;
-    for(var i = 0; i < childs.length; i++) {
+    for (var i = 0; i < childs.length; i++) {
         if (childs[i].nodeName == tagName)
             return childs[i];
     }
@@ -11,12 +10,12 @@ function getFirstChildWithTagName(element, tagName) {
 
 function clearArea(area) {
     if (area == 'definition') {
-	document.getElementById('content').innerHTML = '';
-	document.getElementById('pron').innerHTML = '';
-	document.getElementById('sound').innerHTML = '';
-	document.getElementById('zh_trans').innerHTML = '';
-	document.getElementById('en_trans').innerHTML = '';
-	return null;
+        document.getElementById('content').innerHTML = '';
+        document.getElementById('pron').innerHTML = '';
+        document.getElementById('sound').innerHTML = '';
+        document.getElementById('zh_trans').innerHTML = '';
+        document.getElementById('en_trans').innerHTML = '';
+        return null;
     }
     document.getElementById(area).innerHTML = '';
 }
@@ -25,9 +24,9 @@ function clearArea(area) {
 function showTips(message) {
     var tips = document.getElementById('tips');
     if (message.length == 0)
-	clearArea('tips');
+        clearArea('tips');
     else
-	tips.innerHTML = '<p>' + message + '</p>';
+        tips.innerHTML = '<p>' + message + '</p>';
 }
 
 
@@ -70,7 +69,7 @@ function checkLoginStatus() {
     var request = new XMLHttpRequest();
     var check_url = 'http://www.shanbay.com/api/user/info/';
     request.open('GET', check_url);
-    request.onreadystatechange = function () {
+    request.onreadystatechange = function() {
         if (request.readyState === 4) {
             // user have logged in
             if (request.getResponseHeader('Content-Type') == 'application/json') {
@@ -95,19 +94,19 @@ function addWord() {
     var add_url = 'http://www.shanbay.com/api/learning/add/' + a.title;
     jump.innerHTML = '添加中...';
     request.open('GET', add_url);
-    request.onreadystatechange = function () {
+    request.onreadystatechange = function() {
         if (request.readyState === 4) {
             var learning_id = JSON.parse(request.responseText).id;
-	    clearArea('jump');
+            clearArea('jump');
             jump.appendChild(document.createTextNode('已添加，'));
             var check_link = 'http://www.shanbay.com/review/learning/';
             var check = document.createElement('a');
-	    check.setAttribute('id', 'jump_a');
+            check.setAttribute('id', 'jump_a');
             check.setAttribute('href', check_link + learning_id);
             check.setAttribute('target', '_newtab');
             check.appendChild(document.createTextNode('查看'));
             jump.appendChild(check);
-	    document.getElementById('input').focus();
+            document.getElementById('input').focus();
         }
     };
     request.send(null);
@@ -117,17 +116,17 @@ function addWord() {
 function showEnDefinitions(en_definitions) {
     var en_trans = document.getElementById('en_trans');
     for (var i in en_definitions) {
-	var div = document.createElement('div');
-	div.setAttribute('class', 'part-of-speech');
-	div.innerHTML = '<strong>' + i + '</strong>';
-	var ol = document.createElement('ol');
-	for (var j = 0; j < en_definitions[i].length; j++) {
-	    var li = document.createElement('li');
-	    li.innerText = en_definitions[i][j];
-	    ol.appendChild(li);
-	}
-	en_trans.appendChild(div);
-	en_trans.appendChild(ol);
+        var div = document.createElement('div');
+        div.setAttribute('class', 'part-of-speech');
+        div.innerHTML = '<strong>' + i + '</strong>';
+        var ol = document.createElement('ol');
+        for (var j = 0; j < en_definitions[i].length; j++) {
+            var li = document.createElement('li');
+            li.innerText = en_definitions[i][j];
+            ol.appendChild(li);
+        }
+        en_trans.appendChild(div);
+        en_trans.appendChild(ol);
     }
 }
 
@@ -139,8 +138,8 @@ function queryOk(response) {
     var storage = localStorage.getItem('options');
     // first time run, set localStorage to default
     if ((storage == null) || (storage.search('definitions') == -1)) {
-	storage = 'zh_definitions';
-	localStorage.setItem('options', storage);
+        storage = 'zh_definitions';
+        localStorage.setItem('options', storage);
     }
 
     var learning_id = response.learning_id;
@@ -150,58 +149,76 @@ function queryOk(response) {
     var content = document.getElementById('content');
     content.innerHTML = voc.content + ' ';
     if (voc.pron.length != 0) {
-	var pron = document.getElementById('pron');
-	// if word too long, put pronouncation in the next line
-	if (voc.content.length > 11)
+        var pron = document.getElementById('pron');
+        // if word too long, put pronouncation in the next line
+        if (voc.content.length > 11)
             pron.innerHTML = '<br />[' + voc.pron + ']';
-	else
-	    pron.innerHTML = '[' + voc.pron + '] ';
+        else
+            pron.innerHTML = '[' + voc.pron + '] ';
     }
 
     // if audio is available
     if (voc.audio.length != 0) {
-	var alt = voc.content;
+        var alt = voc.content;
         var img = document.createElement('img');
         img.setAttribute('src', 'static/audio.png');
         img.setAttribute('id', 'horn');
-	img.setAttribute('alt', alt);
-	var sound = document.getElementById('sound');
+        img.setAttribute('alt', alt);
+        var sound = document.getElementById('sound');
         sound.appendChild(img);
-	// if auto play sound option is set
-	if (storage.search('auto') != -1)
-	    playSound();
+        // if auto play sound option is set
+        if (storage.search('auto') != -1)
+            playSound();
     }
 
     // whether show chinese definition
     if (storage.search('zh_definitions') != -1) {
-	var zh_trans = document.getElementById('zh_trans');
-	zh_trans.innerHTML = voc.definition;
+        var zh_trans = document.getElementById('zh_trans');
+        zh_trans.innerHTML = voc.definition;
     }
 
     // whether show english definition
     if (storage.search('en_definitions') != -1)
-	showEnDefinitions(voc.en_definitions);
+        showEnDefinitions(voc.en_definitions);
 
     // jump area
     var jump = document.getElementById('jump');
     if (learning_id != 0) {
         var check_link = 'http://www.shanbay.com/review/learning/';
         var check = document.createElement('a');
-	check.setAttribute('id', 'jump_a');
+        check.setAttribute('id', 'jump_a');
         check.setAttribute('href', check_link + learning_id);
         check.setAttribute('target', '_newtab');
         check.appendChild(document.createTextNode('查看'));
         jump.appendChild(check);
-    }
-    else {
+    } else {
         var add = document.createElement('a');
-	add.setAttribute('id', 'jump_a');
+        add.setAttribute('id', 'jump_a');
         add.setAttribute('href', '#');
         // let addWord function can access the word name by title name
         add.setAttribute('title', voc.content);
         add.appendChild(document.createTextNode('添加到生词本'));
         jump.appendChild(add);
     }
+}
+
+function bdQueryOk(response) {
+    // clear tips area
+    showTips('');
+    // check localStorage
+    var storage = localStorage.getItem('options');
+    // first time run, set localStorage to default
+    if ((storage == null) || (storage.search('definitions') == -1)) {
+        storage = 'zh_definitions';
+        localStorage.setItem('options', storage);
+    }
+
+    var learning_id = response.learning_id;
+    var voc = response.trans_result[0];
+
+    // word and pronouncation
+    var content = document.getElementById('content');
+    content.innerHTML = voc.dst + ' ';
 }
 
 function queryNotFound(word) {
@@ -218,7 +235,7 @@ function query(word) {
     var request = new XMLHttpRequest();
     var query_url = 'http://www.shanbay.com/api/word/' + word;
     request.open('GET', query_url);
-    request.onreadystatechange = function () {
+    request.onreadystatechange = function() {
         if (request.readyState === 4) {
             var response = JSON.parse(request.responseText);
             if (response.voc != '')
@@ -230,16 +247,61 @@ function query(word) {
     request.send(null);
 }
 
+function baiduQuery(word) {
+    // show this let user don't panic
+    showTips('查询中...');
+    clearArea('jump');
+    clearArea('definition');
+
+    var appid = '20161206000033541';
+    var key = '1jo60WSANmevjlNJM2qD';
+    var salt = (new Date).getTime();
+    var query = word;
+    // 多个query可以用\n连接  如 query='apple\norange\nbanana\npear'
+    var from = 'zh';
+    var to = 'en';
+    var str1 = appid + query + salt + key;
+    var sign = MD5(str1);
+
+    var request = new XMLHttpRequest();
+    var query_url = 'http://api.fanyi.baidu.com/api/trans/vip/translate?q=' + word + '&appid=' + appid + '&salt=' + salt + '&from=' + from + '&to=' + to + '&sign=' + sign;
+    request.open('GET', query_url);
+    request.onreadystatechange = function() {
+        if (request.readyState === 4) {
+            console.log(request.responseText)
+            var response = JSON.parse(request.responseText);
+
+            if (response.trans_result != '')
+                bdQueryOk(response);
+            else
+                queryNotFound(word);
+        }
+    }
+    request.send(null);
+}
+
+
+
 function parse(input) {
     var re = /[^a-zA-Z ]+/g;
     input = input.replace(re, '');
     if (input.length == 0 || input.search(/^ +$/) != -1)
-	// have no valid character 
-	return null;
+    // have no valid character 
+        return null;
     else {
-	var word = input.replace(/ +/, ' ');
-	word = word.replace(/^ +| +$/, '');
-	return word;
+        var word = input.replace(/ +/, ' ');
+        word = word.replace(/^ +| +$/, '');
+        return word;
+    }
+}
+
+function isEnglish(input) {
+    var re = /[^a-zA-Z ]+/g;
+    input = input.replace(re, '');
+    if (input.length == 0 || input.search(/^ +$/) != -1)
+        return false;
+    else {
+        return true;
     }
 }
 
@@ -247,27 +309,31 @@ function click() {
     var input = document.getElementById('input').value;
     var word = parse(input);
     if (word == null) {
-	clearArea('jump');
-	clearArea('definition');
-	showTips('<span class="color">英文字符</span>和<span class="color">空格</span>为有效的关键字，请重新输入');
-    }
-    else
-	query(word);
+        clearArea('jump');
+        clearArea('definition');
+        showTips('<span class="color">英文字符</span>和<span class="color">空格</span>为有效的关键字，请重新输入');
+    } else
+        query(word);
     document.getElementById('input').focus();
 }
 
 function keydown() {
     if (event.keyCode == 13) {
         var input = document.getElementById('input').value;
-	var word = parse(input);
-	if (word == null) {
-	    clearArea('jump');
-	    clearArea('definition');
-	    showTips('<span class="color">英文字符</span>和<span class="color">空格</span>为有效的关键字，请重新输入');
-	}
-	else
-	    query(word);
+        var word = parse(input);
+        if (word == null) {
+            baiduQuery(input);
+            clearArea('jump');
+            clearArea('definition');
+            showTips('<span class="color">转百度翻译英文字符</span>和<span class="color">空格</span>为有效的关键字，请重新输入');
+        } else
+            query(word);
     }
+}
+
+function change() {
+    var input = document.getElementById('input').value;
+    alert(isEnglish(input));
 }
 
 function playSound() {
@@ -289,10 +355,12 @@ function playSound() {
     document.getElementById('input').focus();
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     checkLoginStatus();
     document.querySelector('button').addEventListener('click', click);
     document.querySelector('input').addEventListener('keydown', keydown);
+    //document.querySelector('input').addEventListener('input', change);
+    //document.querySelector('input').addEventListener('change', change);
     document.querySelector('#jump').addEventListener('click', addWord);
     document.querySelector('#sound').addEventListener('click', playSound);
     document.querySelector('#sound').addEventListener('mouseover', playSound);
